a8062b8f57f75950be2765dc4b9417dc7f1315c7
hCode122 <hCode122@proton.me>
2025-12-06 22:41:09 +0300

    feat(stock): added logic to update top_losers and most_actively_traded tables

--- server/src/services/dataRefreshService.ts
+++ server/src/services/dataRefreshService.ts
@@ -6,17 +6,25 @@ import { pool } from "../config/database";
 const updateTopChanges = async () => {
     try {
         const response = await alphaVintageInstance.get('/query?function=TOP_GAINERS_LOSERS')
-        const {last_updated, top_gainers, top_losers, most_active} : top_changed_response = response.data 
+        const {last_updated, top_gainers, top_losers, most_actively_traded} : top_changed_response = response.data 
 
         pool.connect()
 
-        for (const stock of top_gainers) {
-            await pool.query(`INSERT INTO top_gainers 
+        
+        for (const stock of top_losers) {
+            await pool.query(`INSERT INTO top_losers 
             (ticker, price, change_amount, change_percentage, volume, last_updated) VALUES
             ($1, $2, $3, $4, $5, $6)`, [stock.ticker, stock.price, stock.change_amount, stock.change_percentage.slice(0, -2), stock.volume, last_updated])
         }
+          
+        for (const stock of most_actively_traded) {
+            await pool.query(`INSERT INTO most_traded 
+            (ticker, price, change_amount, change_percentage, volume, last_updated) VALUES
+            ($1, $2, $3, $4, $5, $6)`, [stock.ticker, stock.price, stock.change_amount, stock.change_percentage.slice(0, -2), stock.volume, last_updated])
+        }
+        
 
-        console.log("Updated the database succesfully at", Date.toLocaleString())
+        console.log(`Database updated at ${new Date().toLocaleString()}`);
     } catch (error) {
         throw error;
     }
