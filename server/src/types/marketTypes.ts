73c929806fb65d7eeb55ed15743a8fddb6155f3b
hCode122 <hCode122@proton.me>
2025-12-07 11:08:07 +0300

    feat(types): added types for AlphaVintage market_overview endpoint response

--- server/src/types/marketTypes.ts
+++ server/src/types/marketTypes.ts
@@ -13,3 +13,16 @@ export interface top_changed_response {
     top_losers: top_changed[];
     most_actively_traded: top_changed[]
 }
+
+export interface market_overview {
+    market_type: string;
+    region: string;
+    primary_exchange: string;
+    local_open: string;
+    local_close: string;
+    current_status: string;
+}
+
+export interface market_overview_response {
+    markets: market_overview[] 
+}
