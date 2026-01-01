4e510b2f7c59f4125467ad98c1306d152536beec
hocde122 <hcode122@proton.me>
2026-01-01 12:44:32 +0300

    feat(database): added type for market calculation endpoint data

--- server/src/scripts/initDatabase.ts
+++ server/src/scripts/initDatabase.ts
@@ -2,6 +2,8 @@ import {pool} from "../config/database";
 
 export const initDatabase = async () => {
     try {
+          await pool.connect()
+
         await pool.query(`
             CREATE TABLE IF NOT EXISTS users (
                 id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -18,7 +20,7 @@ export const initDatabase = async () => {
             CREATE TABLE IF NOT EXISTS portfolios (
                 id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
                 user_id UUID REFERENCES users(id) ON DELETE CASCADE,
-                stock_symbol VARCHAR(10) NOT NULL,
+                stock_symbol VARCHAR(25) NOT NULL,
                 quantity DECIMAL NOT NULL,
                 purchase_price DECIMAL NOT NULL,
                 purchase_date DATE DEFAULT CURRENT_DATE,
@@ -30,7 +32,7 @@ export const initDatabase = async () => {
             CREATE TABLE IF NOT EXISTS watchlists (
                 id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
                 user_id UUID REFERENCES users(id) ON DELETE CASCADE,
-                stock_symbol VARCHAR(10) NOT NULL,
+                stock_symbol VARCHAR(25) NOT NULL,
                 added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                 UNIQUE(user_id, stock_symbol)
             );
@@ -39,7 +41,7 @@ export const initDatabase = async () => {
         await pool.query(`
       CREATE TABLE IF NOT EXISTS top_gainers (
          id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-        ticker VARCHAR(10) NOT NULL,
+        ticker VARCHAR(25) NOT NULL,
         price DECIMAL NOT NULL,
         change_amount DECIMAL NOT NULL,
         change_percentage DECIMAL NOT NULL,
@@ -51,7 +53,7 @@ export const initDatabase = async () => {
     await pool.query(`
       CREATE TABLE IF NOT EXISTS top_losers (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-        ticker VARCHAR(10) NOT NULL,
+        ticker VARCHAR(25) NOT NULL,
         price DECIMAL NOT NULL,
         change_amount DECIMAL NOT NULL,
         change_percentage DECIMAL NOT NULL,
@@ -63,7 +65,7 @@ export const initDatabase = async () => {
     await pool.query(`
       CREATE TABLE IF NOT EXISTS most_traded (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-        ticker VARCHAR(10) NOT NULL,
+        ticker VARCHAR(25) NOT NULL,
         price DECIMAL NOT NULL,
         change_amount DECIMAL NOT NULL,
         change_percentage DECIMAL NOT NULL,
@@ -76,7 +78,7 @@ export const initDatabase = async () => {
       CREATE TABLE IF NOT EXISTS cryptocurrencies (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
         coin_id VARCHAR(50) NOT NULL,
-        symbol VARCHAR(10) NOT NULL,
+        symbol VARCHAR(25) NOT NULL,
         name VARCHAR(100) NOT NULL,
         current_price DECIMAL NOT NULL,
         price_change_percentage_24h DECIMAL,
@@ -89,22 +91,63 @@ export const initDatabase = async () => {
     await pool.query(`
       CREATE TABLE IF NOT EXISTS market_overview (
         id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-        data_type VARCHAR(50) NOT NULL, -- 'global_status', 'sector_performance', 'market_indices'
-        data JSONB NOT NULL, -- Store complete API response
-        last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        market_type Varchar(25) NOT NULL,
+        region VARCHAR(25) NOT NULL,
+        primary_exchanges VARCHAR(100) NOT NULL,
+        local_open VARCHAR(25) NOT NULL,
+        local_close VARCHAR(25) NOT NULL,
+        current_status VARCHAR(25) NOT NULL
       );
     `);
+
+       await pool.query(`
+          CREATE TABLE IF NOT EXISTS etfs (
+            etf_id SERIAL PRIMARY KEY NOT NULL,
+            symbol VARCHAR(25) UNIQUE
+          )
+        `);
+
+      await pool.query(`
+          CREATE TABLE IF NOT EXISTS market_calc (
+            id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+            calculation VARCHAR(25) NOT NULL,
+            value DOUBLE PRECISION NOT NULL,
+            etf_id INTEGER REFERENCES etfs(etf_id) ON DELETE CASCADE,
+
+            UNIQUE(etf_id, calculation)
+            );
+      `);
+
+   
+
+      await pool.query(`
+          CREATE TABLE IF NOT EXISTS stock_metadata (
+            id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+            symbols TEXT[] NOT NULL,
+            min_dt DATE,
+            max_dt DATE,
+            ohlc TEXT,
+            interval VARCHAR(25)
+          )
+        `)
 
     await pool.query(`
       CREATE INDEX IF NOT EXISTS idx_top_gainers_symbol ON top_gainers(ticker);
       CREATE INDEX IF NOT EXISTS idx_top_losers_symbol ON top_losers(ticker);
       CREATE INDEX IF NOT EXISTS idx_cryptocurrencies_symbol ON cryptocurrencies(symbol);
-      CREATE INDEX IF NOT EXISTS idx_market_overview_type ON market_overview(data_type);
+      CREATE INDEX IF NOT EXISTS idx_market_overview_type ON market_overview(market_type);
     `);
 
     console.log('All database tables created successfully');
   } catch (error) {
     console.error('Error initializing database:', error);
     throw error;
+  } finally {
+    await pool.end()
   }
 };
+if (require.main === module) {
+  initDatabase()
+    .then(() => process.exit(0))
+    .catch(() => process.exit(1));
+}
