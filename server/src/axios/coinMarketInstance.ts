cfd71cc0cbd1025bdcef2199e47c1ec13364e527
hocde122 <hcode122@proton.me>
2026-01-04 21:44:20 +0300

    feat(api): added required Headers to the coinMarket Instance

--- server/src/axios/coinMarketInstance.ts
+++ server/src/axios/coinMarketInstance.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
-import { envConfig } from "@/config/environment";
+import { envConfig } from "../config/environment";
 
 
 export const coinMarketInstance = axios.create({
-    baseURL: "pro-api.coinmarketcap.com/v1/cryptocurrency/",
+    baseURL: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/",
     timeout: 30000,
     headers: {
-        "X-CMC_PRO_API_KEY": envConfig.apis.coinMarket
+        "X-CMC_PRO_API_KEY": envConfig.apis.coinMarket,
+        'Accept': "application/json",
+        "Accept-Encoding": "deflate, gzip"
     }
 })
