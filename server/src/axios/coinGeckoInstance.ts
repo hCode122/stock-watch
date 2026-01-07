6967e6b22bee323ca0e990abccc0752eff1748e2
hocde122 <hcode122@proton.me>
2026-01-07 11:28:26 +0300

    feat(api): created axios instance for the coinGecko api

---
+++ server/src/axios/coinGeckoInstance.ts
@@ -0,0 +1,11 @@
+import { envConfig } from "@/config/environment";
+import axios from "axios";
+
+export const coinGeckoInstance = axios.create({
+    baseURL: "https://api.coingecko.com/api/",
+    timeout: 30000,
+    headers: {
+        'x-cg-demo-api-key': envConfig.apis.coinGecko,
+        'Accept': "application/json",
+    }
+})
