f230ae037af54bd04266b48e5fbbffd90aa163a1
hocde122 <hcode122@proton.me>
2026-01-08 21:11:54 +0300

    feat(api): removed version number for coinMarketCap axios instance base url since different calls may need to specify a certain version number to get the right data

--- server/src/axios/coinMarketInstance.ts
+++ server/src/axios/coinMarketInstance.ts
@@ -3,7 +3,7 @@ import { envConfig } from "../config/environment";
 
 
 export const coinMarketInstance = axios.create({
-    baseURL: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/",
+    baseURL: "https://pro-api.coinmarketcap.com/",
     timeout: 30000,
     headers: {
         "X-CMC_PRO_API_KEY": envConfig.apis.coinMarket,
