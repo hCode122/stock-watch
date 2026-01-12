b7d33cf6ee0d2000a308790739d4cc5b931bd387
hocde122 <hcode122@proton.me>
2026-01-12 15:48:19 +0300

    feat(stock): added getStockMarketData to get market overview data from the database

--- server/src/services/marketService.ts
+++ server/src/services/marketService.ts
@@ -45,3 +45,23 @@ export const getTopStockData = async () => {
         console.log(error)
     }
 }
+
+export const getStockMarketData = async () => {
+    try {
+        const data = await pool.query(`
+            SELECT id,
+            market_type,
+            region,
+            primary_exchanges,
+            local_open,
+            local_close,
+            current_status,
+            date::TEXT
+            FROM market_overview
+            WHERE date = (SELECT MAX(date) from market_overview)
+        `)
+        return data.rows;
+    } catch (error) {
+        console.log(error)
+    }
+}
