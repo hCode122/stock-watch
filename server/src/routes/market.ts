eedaa26372f69fcab18a88dc2e019ef7be586b9e
hocde122 <hcode122@proton.me>
2026-01-12 20:15:58 +0300

    feat(api): add /stockCalc endpoint
    - Calls getStockCalcData to get stock calculations like Standard diviation and mean

--- server/src/routes/market.ts
+++ server/src/routes/market.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { getStockMarketOV } from "../controllers/marketController";
+import { getStockMarketOV, getStockCalcData, getLatestCoinsData } from "../controllers/marketController";
 
 const router = Router();
 router.use('/marketStock', getStockMarketOV);
+router.use('/stockCalc', getStockCalcData);
+
 export default router;
