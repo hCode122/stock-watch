770e9c9ded17f6b4798d6cc33d41635db2e58083
hocde122 <hcode122@proton.me>
2026-01-19 14:39:20 +0300

    feat(font): replaced geist font with the Robot font

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,15 +1,14 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
+import { ReduxProvider } from "./ReduxProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'], 
+  subsets: ['latin'],
+  display: 'swap', 
+  variable: '--font-roboto', 
 });
 
 export const metadata: Metadata = {
@@ -25,9 +24,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto.variable} ${roboto.variable} antialiased`}
       >
-        {children}
+      <ReduxProvider>
+                {children}
+      </ReduxProvider>
       </body>
     </html>
   );
