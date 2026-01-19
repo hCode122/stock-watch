5b79de870640b6c7c082d2a9b9d86e03dcd09b1d
hocde122 <hcode122@proton.me>
2026-01-19 14:42:55 +0300

    feat(redux): created ReduxProvider
    - Allows all  children components to access the global state store

---
+++ frontend/app/ReduxProvider.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { store } from "./state/store";
+import { Provider } from "react-redux";
+
+export const ReduxProvider = ({
+    children
+}: {children: React.ReactNode}) => {
+    return (
+        <Provider store={store}>
+            {children}
+        </Provider>
+    );
+}
