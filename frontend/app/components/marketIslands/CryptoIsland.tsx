4c54e1adb63a46e32cc0315ee6328c88679f9651
hocde122 <hcode122@proton.me>
2026-03-13 11:12:42 +0300

    feat(components): added button to buy a certain stock or coin

--- frontend/app/components/marketIslands/CryptoIsland.tsx
+++ frontend/app/components/marketIslands/CryptoIsland.tsx
@@ -1,15 +1,23 @@
 'use client'
 
+import { useState } from "react"
 import { useGetLatestCoin } from "@/hooks/useGetLatestCoin"
 import LatestCoinsCard from "../StatCards/LatestCoinsCard"
 import { useGetCoinMarketOvData } from "@/hooks/useGetCoinMarketOvData"
 import CoinMarketOvCard from "../StatCards/CoinMarketOvCard"
 import { Button } from "@/components/ui/button"
 import { ArrowBigUp } from "lucide-react"
+import BuyDialog from "../misc/BuyDialog"
+
+interface stockBuyData {
+    ticker: string,
+    price: number
+}
 
 const CryptoIsland = () => {
     const {coinData, coinLoading, coinError} = useGetLatestCoin()
     const {coinMarketData, coinMarketLoading, coinMarketError} = useGetCoinMarketOvData()
+    const [buy, setBuy] = useState<stockBuyData | null>(null)
 
         const scrollToSection = (id: string) => {
         const element = document.getElementById(id);
@@ -24,8 +32,11 @@ const CryptoIsland = () => {
     return (
         <div id="stock" className="flex-col w-full gap-4 mt-14">
             <h2 className="text-lg font-[700]">Crypto Market</h2>
+              {
+                buy != null ? <BuyDialog setBuy={setBuy} purchase_type="coin" stock={buy}/> : null
+            }
             <div className="flex max-w-full flex-col lg:flex-row justify-evenly gap-4">
-                    <LatestCoinsCard coinData={coinData} coinError={coinError} coinLoading={coinLoading} />
+                    <LatestCoinsCard setBuy={setBuy} coinData={coinData} coinError={coinError} coinLoading={coinLoading} />
                     <CoinMarketOvCard coinMarketData={coinMarketData} coinMarketLoading={coinMarketLoading}
                     coinMarketError={coinMarketError} />
             </div>
--- frontend/app/components/marketIslands/StockIsland.tsx
+++ frontend/app/components/marketIslands/StockIsland.tsx
@@ -9,6 +9,12 @@ import { useGetStockCalcData } from "@/hooks/useGetStockCalcData"
 import StockChart from "../StatCards/StockChartCard"
 import { useGetStockChartData } from "@/hooks/useGetStockChartData"
 import { useState } from "react"
+import BuyDialog from "../misc/BuyDialog"
+
+interface stockBuyData {
+    ticker: string,
+    price: number
+}
 
 const StockIsland = () => {
     const {topStockData, loading, error} =  useGetTopStocks()
@@ -16,14 +22,19 @@ const StockIsland = () => {
     const {calcData, calcLoading, calcError} = useGetStockCalcData()
     const [selectedStock, setSelectedStock] = useState('NAMMW')
     const {stockChartData, chartLoading, chartError} = useGetStockChartData(selectedStock)
+    const [buy, setBuy] = useState<stockBuyData | null>(null)
+    
 
     const stocks: string[] | undefined = topStockData?.topGainers.map(stock => stock.ticker)
     return (
         <div id="stock" className="flex-col w-full mt-12 gap-4 ">
             <h2 className="text-lg font-[700]">Stock Market</h2>
+            {
+                buy != null ? <BuyDialog setBuy={setBuy} purchase_type="stock" stock={buy}/> : null
+            }
             <div className="flex max-w-full flex-col lg:flex-row justify-evenly gap-4">
-                <StockChangeCard data={topStockData?.topGainers} type='gainer' loading={loading} error={error} />
-                <StockChangeCard data={topStockData?.topLosers} type='looser' loading={loading} error={error}  />
+                <StockChangeCard setBuy={setBuy} data={topStockData?.topGainers} type='gainer' loading={loading} error={error} />
+                <StockChangeCard setBuy={setBuy} data={topStockData?.topLosers} type='looser' loading={loading} error={error}  />
             </div>
             <div className="lg:grid lg:grid-cols-7 gap-4">
                 <StockMarketOvCard data={stockMarketOV} loading={marketLoading} error={marketError} />
