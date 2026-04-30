9a19aa1899715d6c62c4f1d461e64bbf9f887c59
hocde122 <hcode122@proton.me>
2026-04-30 20:57:54 +0300

    choreL environment.ts template can now be uploaded

---
+++ server/src/config/environment.ts
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const envConfig = {
+    port: process.env.PORT || 5005,
+    nodeEnv: process.env.NODE_ENV || 'development',
+
+    database: {
+        host: process.env.DB_HOST || 'localhost',
+        port: parseInt(process.env.DB_PORT || '5432'),
+        name: process.env.DB_NAME || 'stock-app',
+        user: process.env.DB_USER || 'postgres',
+        password: process.env.DB_PASSWORD || '', 
+    },
+    
+    jwt: {
+        secret: process.env.JWT_SECRET || '', 
+        expiresIn: process.env.JWT_EXPIRES_IN || '7d',
+    },
+    
+    apis: {
+        alphaVantage: process.env.ALPHA_VANTAGE_API_KEY || '',
+        coinMarket: process.env.COIN_MARKET || '',
+        coinGecko: process.env.COIN_GECKO || '',
+    },
+};
